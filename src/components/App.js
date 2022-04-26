cb462a5101048f874a63be3a303114a5d6aae642
Aran Diez <99818791+AranDiez@users.noreply.github.com>
2022-04-26 10:26:54 +0200

    added filter possibility for quote

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,21 @@ import { useState } from 'react';
 function App() {
   // 3 - Variable de estado con la lista de quotes
   const [data, setData] = useState(quoteList);
-  // 4 - creo la constante y hago map de data para que no solo renderice una vez. Hago el map para que me pinte el array (la lista). Hago tambien key para filtrar luego.
-  const htmlData = data.map((quote, i) => (
-    <li key={i}>
-      <p>
-        {quote.quote} {quote.character}
-      </p>
-    </li>
-  ));
+
+  // map
+  // const htmlData = data.map((quote, i) => (
+  //   <li key={i}>
+  //     <p>
+  //       {quote.quote} {quote.character}
+  //     </p>
+  //   </li>
+  // ));
+  // filter
+  // const htmlData = data.filter(
+  //   (quote) =>
+  //     quote.name.toLowerCase().includes(search.toLowerCase()) ||
+  //     quote.lastname.toLowerCase().includes(search.toLowerCase())
+  // );
   // 5 - Añadir nuevos contactos. Creo variable de estado objeto.
   const [newQuote, setNewQuote] = useState({
     quote: '',
@@ -37,10 +44,54 @@ function App() {
       character: '',
     });
   };
+  // 8 - Creo una variable de estado que esté pendiente del filtrado
+  const [search, setSearch] = useState('');
+  // 9 - Hago un evento que pille lo que se escribe. He puesto un onchange en el input que la llama.
+  const handleSearch = (ev) => {
+    setSearch(ev.target.value);
+  };
+  // 4 - creo la constante y hago map de data para que no solo renderice una vez. Hago el map para que me pinte el array (la lista). Hago tambien key para filtrar luego.
+  // 10 - Meto en la variable de estado el valor del input
+
+  const htmlData = data
+    .filter(
+      (quote) =>
+        quote.quote.toLowerCase().includes(search.toLowerCase()) ||
+        quote.character.toLowerCase().includes(search.toLowerCase())
+    )
+    .map((quote, i) => (
+      <li key={i}>
+        <p>
+          {quote.quote} {quote.character}
+        </p>
+      </li>
+    ));
+
   return (
     <div className="App">
       <section>
         <h1>Frases de friends</h1>
+        <form>
+          <label htmlFor="filterText"> Filtrar por frase </label>
+          <input
+            type="text"
+            name="filterText"
+            id="filterText"
+            placeholder="Filtrar"
+            onChange={handleSearch}
+            // value={search}
+          />
+          <label htmlFor="filterCha"> Filtrar por personaje </label>
+          <select name="filterCha" id="filterCha">
+            <option>Todos</option>
+            <option>Ross</option>
+            <option>Monica</option>
+            <option>Joey</option>
+            <option>Phoebe</option>
+            <option>Chandler</option>
+            <option>Rachel</option>
+          </select>
+        </form>
       </section>
       <section>
         <ul>{htmlData}</ul>
